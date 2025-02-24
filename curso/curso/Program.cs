af6fb0424a1a2060b9b89a3912cdd36fc676b5f8
Ana Júlia Leite <leite.anajulia25@gmail.com>
2025-02-24 01:31:24 -0300

    atualizando adição de conteúdo

---
+++ curso/curso/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace curso
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            byte n1 = 126;
+            int n2 = 1000;
+            int n3 = 2147483647;
+            long n4 = 2147483648;
+
+            n1++;
+
+            Console.WriteLine(n1);
+            Console.WriteLine(n2);
+            Console.WriteLine(n3);
+            Console.WriteLine(n4);
+        }
+    }
+}
---
+++ usuario/usuario/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace usuario
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Ana Júlia de Lima Aguiar Leite";
+            int quarto = 3;
+            double preco = 500.50;
+            string lastName = "Leite";
+            int idade = 32;
+            double altura = 1.61;
+
+            Console.WriteLine(name);
+            Console.WriteLine(quarto);
+            Console.WriteLine(preco);
+            Console.WriteLine($"{lastName} {idade} {altura}");
+        }
+    }
+}
